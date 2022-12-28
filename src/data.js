9dfd095f730678b328a3399834765d29de96bdd6
Zhenkaaf <zhenkaaf@gmail.com>
2022-12-28 12:44:45 +0200

    up

--- src/data.js
+++ src/data.js
@@ -2,17 +2,17 @@ export const products = [
     {
         id: 1,
         img: 'https://i.pinimg.com/564x/d8/34/f7/d834f76819eb063c66bf66a3d78035b9.jpg',
-        link: 'http://lama.dev'
+        link: 'https://zhenkaaf.github.io/myPortfolioFODOROV/'
     },
     {
         id: 2,
         img: 'https://images.moviesanywhere.com/f6615eb21ab15d7f3a7e6166dec9dad9/8e53b06a-3222-4367-a13c-e05dbaf215ba.jpg',
-        link: 'http://lama.dev'
+        link: 'https://zhenkaaf.github.io/myPortfolioFODOROV/'
     },
     {
         id: 3,
         img: 'https://i.pinimg.com/originals/93/ea/e6/93eae65ea108edf4935cc3aed4562d82.jpg',
-        link: 'http://lama.dev'
+        link: 'https://zhenkaaf.github.io/myPortfolioFODOROV/'
     },
     {
         id: 4,
@@ -22,11 +22,11 @@ export const products = [
     {
         id: 5,
         img: 'https://cineuropa.org/Galleries/088/094/poster1_de_big.jpg?1470604273658',
-        link: 'http://lama.dev'
+        link: 'https://zhenkaaf.github.io/myPortfolioFODOROV/'
     },
     {
         id: 6,
         img: 'https://i.pinimg.com/564x/ee/6a/42/ee6a425c99f64c21349539cb2092f93b.jpg',
-        link: 'http://lama.dev'
+        link: 'https://zhenkaaf.github.io/myPortfolioFODOROV/'
     },
 ]
